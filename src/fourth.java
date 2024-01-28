2a539f1d86bf2bdd13a9d54e76c28abc283f2d85
Ramis <sir.ramis@mail.ru>
2024-01-28 10:49:29 +0300

    Added 1

---
+++ src/fourth.java
@@ -0,0 +1,24 @@
+public class fourth {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 6;
+
+        int rez1;
+        int rez2;
+        int rez3;
+        int rez4;
+        int rez5;
+        System.out.println(rez1 = a + b);
+        System.out.println(rez2 = a * b);
+        System.out.println(rez3 = a - b);
+        System.out.println(rez4 = a / b);
+        System.out.println(rez5 = a % b);
+
+        System.out.println(rez1 % 2);
+        System.out.println(rez2 % 2);
+        System.out.println(rez3 % 2);
+        System.out.println(rez4 % 2);
+        System.out.println(rez5 % 2);
+
+    }
+}
